ca12e7ed9aa3cf6df0eb5eb3c48c02eef134e305
qaiik <qaiikprogramming@gmail.com>
2025-09-26 11:19:34 -0600

    cute

--- src/lib/AudioEngine/classes/track.ts
+++ src/lib/AudioEngine/classes/track.ts
@@ -1,6 +1,6 @@
 import { link } from "original-fs";
 import { colorAsHex, RGBColorInterface } from "../../ui/interfaces/RGBColorInterface";
-import { linkTrack } from "../../wasm/build/release";
+import { linkTrack, setTrackIsActive } from "../../wasm/build/release";
 import { AutomationTrack } from "./automation_track";
 import { SProject } from "./project";
 import { AudioClipSource } from "./sources/AudioClipSource";
@@ -30,18 +30,32 @@ export class Track {
     uuid: string;
     color: string;
     trackNumId: number;
+    active: boolean;
     constructor(options: TrackOptionsInterface) {
         this.bitDepth = options.bitDepth ?? 32;
         this.automationTracks = [];
 
         baseTrackId++;
-        this.name = options.name?.slice(0,25) ?? 'Track ' + baseTrackId;
+        this.name = options.name?.slice(0, 25) ?? 'Track ' + baseTrackId;
         this.trackNumId = baseTrackId;
         this.uuid = crypto.randomUUID()
         this.color = options.color ? colorAsHex(options.color) : "#300606e5"
+        this.active = false;
     }
 
     setPlugin(plugin: SPlugin) {
-        linkTrack(this.uuid, plugin.pluginId, this.bitDepth, plugin.mediaType)
+        if (plugin.loaded) {
+            return linkTrack(this.uuid, plugin.pluginId, this.bitDepth, plugin.mediaType!)
+        } else {
+            throw new Error("Plugin must be loaded to link to a track.")
+        }
     }
+
+    setActive(active: boolean) {
+        this.active = active;
+        setTrackIsActive(this.uuid, this.active)
+        return this.active
+    }
+
+    
 }
