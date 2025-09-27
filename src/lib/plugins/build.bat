6fe495d036de16201e09e037d877834a6c64d9e0
Winter <qaiikprogramming@gmail.com>
2025-09-26 22:55:59 -0600

    current

--- src/lib/plugins/build.bat
+++ src/lib/plugins/build.bat
@@ -1,1 +1,2 @@
 @asc -o example.wasm example.asc.ts --enable simd --importMemory && move /Y .\example.wasm ..\..\..\public/example.wasm
+@asc -o fullFeatured.wasm fullFeatured.asc.ts --enable simd --importMemory && move /Y .\fullFeatured.wasm ..\..\..\public/fullFeatured.wasm
